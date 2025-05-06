41ac68a347993808714bba849688bb75924093ee
bamidele <devbammy@gmail.com>
2025-05-06 23:12:17 +0100

    feat: refactor styles and layout structure, replace CSS with SCSS, and enhance page content

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,16 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1 className={styles.head}>recognition </h1>
+      <h2>Image Recognition</h2>
+      <h3>Upload an image to recognize</h3>
+      <a href="" className="btn">
+        Link
+      </a>
+      <p>loremfdsvzhjklbfdljkgbdfi gzuidfg zuidrfgh erui fghzuiydsg </p>
+    </>
+  );
 }
