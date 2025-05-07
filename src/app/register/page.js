025e2f9c6ea0c3877f4abdbb8f1d479b50709459
bamidele <devbammy@gmail.com>
2025-05-08 00:21:57 +0100

    feat: implement registration page layout and styles with SCSS

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,20 @@
-const Page = () => {
-  return <section className="page">Page</section>;
-};
+import styles from './register.module.scss';
+
+const Page = () => (
+  <div className={styles.register}>
+    <div className={styles.row}>
+      <div className={styles.col}></div>
+      <div className={styles.col}></div>
+      <h2>Create Account</h2>
+
+      <form>
+        <div className="formControl">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" placeholder="Your Name" />
+        </div>
+      </form>
+    </div>
+  </div>
+);
 
 export default Page;
